328660d00c795d33a8e6295195d688c875b6767b
Egor Maiorov <maiorov27@gmail.com>
2014-08-29 22:15:55 +0300

    WordClass v.1

--- src/main/java/logic/Word.java
+++ src/main/java/logic/Word.java
@@ -1,4 +1,34 @@
 package logic;
 
 public class Word {
+    private final String word;
+
+    public Word(String word){
+        this.word = word;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public boolean equals(Object o){
+        if( o == null)
+            return false;
+        if(!(o instanceof Word))
+            return false;
+        if(o == this)
+            return true;
+        Word that = (Word) o;
+        return that.equals(this);
+    }
+
+    public int hashCode(){
+        int result =31*2+word.hashCode();
+        return result;
+    }
+
+    public String toString(){
+        return word;
+    }
+
 }
